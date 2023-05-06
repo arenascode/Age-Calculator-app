3e508c7c6367e714e6f2795a0cdec06274a9dd6c
Miguel Arenas <migue.arenas91@gmail.com>
2023-05-05 22:02:40 -0500

    mobile design and logic age calculator done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // Result of age calculate
 const yearResult = document.getElementById('showYear')
 const monthResult = document.getElementById('showMonth')
@@ -20,7 +21,12 @@ const yearStyleInput = document.querySelector(
 'input[name="year"]'
 )
 
+//Date
+const date = new Date()
+console.log(date);
+  
 // for apply Error styles if some input is empty
+// Days
 function requiredDay() {
   dayLabel.style.color = "var(--color-light-red)";
   dayStyleInput.style.borderColor =
@@ -34,6 +40,7 @@ function mustBeValidDay() {
   spans[0].style.display = "block";
   spans[0].innerText = `Must be a valid day`
 }
+//Months
 function requiredMonth() {
   monthLabel.style.color =
   "var(--color-light-red)";
@@ -48,6 +55,7 @@ function mustBeValidMonth() {
   spans[1].style.display = "block";
   spans[1].innerText = `Must be a valid month`;
 }
+//Years
 function requiredYear() {
   yearLabel.style.color =
   "var(--color-light-red)";
@@ -62,6 +70,12 @@ function mustBeValidYear() {
   spans[2].style.display = "block";
   spans[2].innerText = `Must be a valid year`;
 }
+function mustBeInThePast() {
+  yearLabel.style.color = "var(--color-light-red)";
+  yearStyleInput.style.borderColor = "var(--color-light-red)";
+  spans[2].style.display = "block";
+  spans[2].innerText = `Must be in the past`;
+}
 
 if (form instanceof HTMLFormElement) {
   function calculateResult(e) {
@@ -71,36 +85,76 @@ if (form instanceof HTMLFormElement) {
     const dayInput = dataForm.get('day')
     const monthInput = dataForm.get('month')
     const yearInput = dataForm.get('year')
-    console.log(dayInput);
-
-    console.log(isNaN(parseInt(dayInput)));
-
-    isNaN(parseInt(dayInput)) ? mustBeValidDay() : console.log(`the date is correct`);
     
+    // invalid day validation
+    isNaN(parseInt(dayInput)) || dayInput < 1 || dayInput > 31 ? mustBeValidDay() : console.log(`the date is correct`);
+    
+    //empty day input validation
     dayInput == "" ? requiredDay() : console.log(`there is data`)
-
-    isNaN(parseInt(monthInput))
-      ? mustBeValidMonth()
-      : console.log(`the date is correct`);
-
-    monthInput == ""
-      ? requiredMonth()
-      : console.log(`there is data`);
     
-    isNaN(parseInt(yearInput))
-      ? mustBeValidYear()
+    // invalid month validation
+    isNaN(parseInt(monthInput)) || monthInput < 1 || monthInput > 12 ? mustBeValidMonth()
       : console.log(`the date is correct`);
     
+    //empty month input validation
+    monthInput == "" ? requiredMonth() : console.log(`there is data`);
+    
+    // invalid year validation
+    isNaN(parseInt(yearInput)) || yearInput < 1 ? mustBeValidYear()
+      : console.log(`the date is correct`);
+    
+    // empty year input validation
     yearInput == ""
       ? requiredYear()
       : console.log(`there is data`);
+    
+    //year future validation
+    yearInput > date.getFullYear() ? mustBeInThePast() : console.log(`año del pasado`);
+    
+    const monthNumber = parseInt(monthInput)
+    if (parseInt(dayInput) === 31 && (monthNumber === 02 || monthNumber === 04 || monthNumber === 06 || monthNumber === 09 || monthNumber === 11)) return mustBeValidDay()
 
-  console.log(`enviado`);
+    if ((parseInt(dayInput) === 30 || parseInt(dayInput) === 31) && monthNumber === 02) return mustBeValidDay()
+    
+    if (
+      isNaN(parseInt(dayInput)) || dayInput < 1 ||
+      dayInput > 31 ||
+      isNaN(parseInt(monthInput)) || monthInput < 1 || parseInt(monthInput) > 12 ||
+      isNaN(parseInt(yearInput)) || yearInput <1 || yearInput > date.getFullYear()
+    ) {
+      console.log(`please complete all fields`);
+    } else {
+      let dateToString = `${yearInput}-${monthInput}-${dayInput}`;
+      const dateEntered = Number(new Date(dateToString));
 
-  const date = new Date()
-  console.log(date);
-  
-}
+      const today = Number(new Date());
+
+      const milisecondInADay = 86400000;
+      const daysInAYear = 365;
+      const daysInaMonth = 31;
+
+      const resultInMiliseconds = today - dateEntered;
+      const resultInDays = resultInMiliseconds / milisecondInADay;
+      console.log(resultInDays);
+      const resultInYears = resultInDays / daysInAYear;
+
+      const calculatedYear = Number(resultInYears.toString().split(".")[0]);
+      const decimalToString = resultInYears.toString().split(".")[1];
+      const decimalValue = Number(`0.${parseInt(decimalToString)}`);
+
+      const monthsToString = ((decimalValue * 365) / 31).toString();
+      const calculatedMonth = Number(monthsToString.split(".")[0]);
+
+      const calculateDaysByDecimal = monthsToString.split(".")[1];
+      const calculatedDays = Math.floor(
+        Number(`0.${parseInt(calculateDaysByDecimal)}`) * 31
+      );
+
+      yearResult.innerText = calculatedYear;
+      monthResult.innerText = calculatedMonth;
+      dayResult.innerText = calculatedDays;
+    }
+  }
 } else {
   console.log(`isn't a form`);
 }
